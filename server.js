462f1337d82c85522676fae9213674067392d868
ujstha <ujstha27@gmail.com>
2020-02-16 21:05:33 +0200

    UI update

--- server.js
+++ server.js
@@ -1,35 +1,43 @@
 // GRAB THE PACKAGES/VARIABLES WE NEED
 // ==================================================
-var express = require('express');
+var express = require("express");
 var app = express();
-var ig = require('instagram-node').instagram();
-const PORT = process.env.PORT || 5000
+var ig = require("instagram-node").instagram();
+const PORT = process.env.PORT || 5000;
 // CONFIGURE THE APP
 // ==================================================
 // tell node where to look for site resources
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 // configure instagram app with your access token
 // we'll get to this soon
 // SET THE ROUTES
 // ===================================================
 // home page route - our profile's images
 // home page route - popular images
-app.get('/', function(req, res) {
-    // use the instagram package to get popular media
-    ig.user_self_media_recent(function(err, medias, pagination, remaining, limit) {
-    // render the home page and pass in the popular images
-    res.render('pages/index', { grams: medias });
+app.get("/", function(req, res) {
+  // use the instagram package to get popular media
+  ig.user_self_media_recent(function(
+    err,
+    medias,
+    pagination,
+    remaining,
+    limit
+  ) {
+    ig.user("2095860555", function(err, result, remaining, limit) {
+      // render the home page and pass in the popular images
+      res.render("pages/index", { grams: medias, user: result });
     });
+  });
 });
 
 // configure instagram app with your access_token
 ig.use({
-    // get access token here: http://instagram.pixelunion.net/
-    access_token: '2095860555.1677ed0.70ce892c20ea410aa9a8ee3a917bf85c',
+  // get access token here: http://instagram.pixelunion.net/
+  access_token: "2095860555.1677ed0.03f0fa0645aa468f9453ee047019cf2d"
 });
-ig.user_self_media_recent(function(err, medias, pagination, remaining, limit) {});
+
 // START THE SERVER
 // ==================================================
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
